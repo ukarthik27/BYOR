0d918faaa73568a273c24398fe371368cc6947af
ukarthik27 <ukarthik27@gmail.com>
2022-12-28 22:52:47 -0500

    Handle functional component, implement useState

--- byor/src/index.js
+++ byor/src/index.js
@@ -25,21 +25,6 @@ function createTextElement(text) {
 // replace React.render(element,container)
 // create the DOM node using the element type, and then append the new node to the container
 
-// function oldrender(element, container) {
-//   const dom =
-//     element.type === "TEXT_ELEMENT"
-//       ? document.createTextNode("")
-//       : document.createElement(element.type);
-//   const isProperty = key => key !== "children";
-//   console.log(element)
-//   Object.keys(element.props)
-//     .filter(isProperty)
-//     .forEach(name => {
-//       dom[name] = element.props[name];
-//     });
-//   element.props.children.forEach(child => render(child, dom));
-//   container.appendChild(dom);
-// }
 
 // rendering deep dom trees recursively is a render blocking process
 // split work into smaller units
@@ -61,6 +46,9 @@ let nextUnitOfWork = null
 let wipRoot = null
 let currentRoot = null
 let deletions = null
+
+let wipFiber = null
+let hookIndex = null
 
 // for event listeners with "on"
 const isEvent = key => key.startsWith("on")
@@ -127,7 +115,13 @@ function commitWork(fiber) {
     return
   }
 
-  const domParent = fiber.parent.dom
+  // since fn comp are fibers without DOM nodes, we need search for fiber with DOM node
+  let domParentFiber = fiber.parent
+  while(!domParentFiber.dom){
+    domParentFiber = domParentFiber.parent
+  }
+  const domParent = domParentFiber.dom
+
   if (
     fiber.effectTag === "PLACEMENT" &&
     fiber.dom != null
@@ -143,18 +137,26 @@ function commitWork(fiber) {
       fiber.props
     )
   } else if (fiber.effectTag === "DELETION") {
-    domParent.removeChild(fiber.dom)
+    commitDeletion(fiber,domParent)
   }
 
   commitWork(fiber.child)
   commitWork(fiber.sibling)
 }
 
+function commitDeletion(fiber,domParent){
+  if(fiber.dom){
+    domParent.removeChild(fiber.dom)
+  } else {
+    commitDeletion(fiber.child,domParent)
+  }
+}
+
 function commitRoot(){
   deletions.forEach(commitWork)
   // add nodes to dom
   commitWork(wipRoot.child)
-  currentRoot = wipRoot //copy of what was commit. Used for Reconcilation
+  currentRoot = wipRoot //copy of what was committed. Used for Reconcilation
   wipRoot = null
 }
 
@@ -254,17 +256,20 @@ function reconcileChildren(wipFiber, elements) {
 
 function performUnitOfWork(fiber){
   // add dom node
-  if (!fiber.dom) {
-    fiber.dom = createDom(fiber)
-  }
-
   // add new node to DOM for each element. But browser could interrupt before we finish
   // we’ll keep track of the root of the fiber tree. - work in progress wipRoot
   
   // create new fibers
-  const elements = fiber.props.children
-  reconcileChildren(fiber, elements)
-
+  // functional components are different
+  // Fiber of fn comp doesnt have DOM node. Have to be called to return DOM.
+  
+  const isFuncComp = fiber.type instanceof Function
+  if(isFuncComp){
+    updateFunctionComponent(fiber)
+  } else {
+    updateHostComponent(fiber)
+  }
+  
 
   // return next unit of work - child, sibiling, uncle
   if (fiber.child) {
@@ -280,10 +285,58 @@ function performUnitOfWork(fiber){
   
   
 }
+function updateFunctionComponent(fiber){
+  wipFiber = fiber
+  hookIndex = 0
+  wipFiber.hooks = []
+  const children = [fiber.type(fiber.props)]
+  reconcileChildren(fiber,children)
+}
 
+function updateHostComponent(fiber){
+  if(!fiber.dom){
+    fiber.dom = createDom(fiber)
+  }
+  reconcileChildren(fiber,fiber.props.children)
+}
+
+function useState(initial){
+  const oldHook = 
+    wipFiber.alternate &&
+    wipFiber.alternate.hooks &&
+    wipFiber.alternate.hooks[hookIndex]
+
+  const hook = {
+    state: oldHook ? oldHook.state : initial,
+    queue: []
+  }
+
+  const actions = oldHook ? oldHook.queue : []
+  actions.forEach(action => {
+    hook.state = action(hook.state)
+  })
+
+  const setState = action => {
+    hook.queue.push(action)
+    wipRoot = {
+      dom: currentRoot.dom,
+      props: currentRoot.props,
+      alternate: currentRoot,
+    }
+    nextUnitOfWork = wipRoot
+    deletions = []
+  }
+  
+  wipFiber.hooks.push(hook)
+  hookIndex++
+  return [hook.state,setState]
+
+}
+// ----------------------------------
 const byor = {
   createElement,
-  render
+  render,
+  useState,
 };
 
 // comment like this one, when babel transpiles the JSX it will use the function we define.
@@ -291,20 +344,26 @@ const byor = {
 
 const container = document.getElementById("root");
 
-const updateValue = e => {
-  rerender(e.target.value)
-}
 
-const rerender = value => {
-  const element = (
+function Counter() {
+  const [state, setState] = byor.useState(1)
+  const [ipState, ipSetState] = byor.useState('there')
+  return (
     <div style="background: lightgreen">
-      <input onInput={updateValue} value={value} />
-      <h2>Hello {value}</h2>
+      <h1>Multiple state values</h1>
+      <h1 onClick={() => setState(c => c + 1)}>
+      Count: {state}
+      </h1>
+      <label>Type your name : </label>
+      <input value={ipState} onChange={(e)=>{ipSetState(()=>e.target.value)}} />
+      <h1>Hello {ipState} !!</h1>
     </div>
+    
   )
-  byor.render(element, container);
 }
+const element = <Counter />
+byor.render(element, container);
 
-rerender("There !!")
+
 
 
